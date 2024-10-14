9df6a54bab6e91bd9311bcb644d7d340863ce2a5
korchel <korchel21@gmail.com>
2024-10-14 13:33:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const PORT = 8080;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(PORT, console.log("PORT" + PORT));
